97bde5c854182490f857915eab59669ac38ccd72
Phenom9834 <pritam.kondapratiwar@opstree.com>
2025-07-22 15:10:12 +0530

    Update main.tf

--- example/main.tf
+++ example/main.tf
@@ -7,5 +7,6 @@ module "gke" {
   use_existing_sa       = var.use_existing_sa
   service_account_email = var.service_account_email
   service_account_id    = var.service_account_id
-  service_account_roles = var.service_account_roles
+  service_account_roles = var.service_account_roles  
+  ssh_keys              = var.ssh_keys
 }
